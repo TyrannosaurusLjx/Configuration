cc06d5c7fc38b5800fccd374b6cf5488f3107449
ljx <junxun-luo@outlook.com>
2024-06-29 13:46:07 +0800

    finder vim

---
+++ scripts/neovide_anywhere.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title neovide_anywhere
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author junxun_luo
+# @raycast.authorURL https://raycast.com/junxun_luo
+
+
+# 获取当前时间
+# time=$(date "+%Y-%m-%d %H:%M:%S")
+#
+# touch ~/.config/temp/${time}.txt
+#
+# # 用 neovide 打开文件
+# neovide ~/.config/temp/${time}.txt
+#
+# # 等待 neovide 关闭文件
+# while [ -f ~/.config/temp/${time}.txt ]; do
+#   sleep 1
+# done
+#
+# # 复制到剪贴板
+# cat ~/.config/temp/${time}.txt | pbcopy
